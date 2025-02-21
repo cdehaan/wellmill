2021b94e5078413953dbadc4a21477547f0c4864
Chris DeHaan <chris@nextvision.co.jp>
2025-02-21 12:38:39 +0900

    Sends search API request

--- src/Pages/admin/CouponSearch.tsx
+++ src/Pages/admin/CouponSearch.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './CouponSearchStyle.css';
 import { CouponType } from '../../types';
+import CallAPI from '../../Utilities/CallAPI';
 
 const token = window.location.search ? new URLSearchParams(window.location.search).get('token') || "" : localStorage.getItem('token') || "";
 
@@ -10,18 +11,30 @@ type CouponSearchProps = {
 };
 
 export default function CouponSearch({language, setShowSearchCoupons}: CouponSearchProps) {
-  const [searchText, setSearchText] = useState<string>("");
+  const [searchString, setSearchString] = useState<string>("");
   const [searchResults, setSearchResults] = useState<CouponType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
   const [coupon, setCoupon] = useState<CouponType | null>(null);
+
+  async function handleSearch() {
+    setIsLoading(true);
+    setSearchResults([]);
+    const requestBody = {
+      token: token,
+      searchString: searchString,
+    };
+    const responseData = await CallAPI(requestBody, 'adminCouponSearch');
+    console.log(responseData);
+    setIsLoading(false);
+  }
+
   return (
     <div className="couponSearchCover">
       <div className='couponSearchModal'>
         <h1>Coupon Search</h1>
         <div className='couponSearchInput'>
-          <input type="text" placeholder="Enter coupon code" />
-          <span className='searchButton'>Search</span>
+          <input type="text" placeholder="Enter coupon code" value={searchString} onChange={(e) => setSearchString(e.target.value)} />
+          <span className='searchButton' onClick={handleSearch}>Search</span>
         </div>
         <button onClick={() => setShowSearchCoupons(false)}>Back</button>
       </div>
